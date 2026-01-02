b87567d40bc92f5a8f8db29679cd949b54e3851c
Rubens_Rabelo <rubensrabelo123@gmail.com>
2026-01-02 17:20:08 -0300

    docs(aes_block): add docstrings

--- src/crypto/aes_block.py
+++ src/crypto/aes_block.py
@@ -1,10 +1,17 @@
 from Crypto.Cipher import AES
 
-# AES como cifra de bloco (ECB)
+
 def aes_encrypt_block(key, block):
+    """
+    Criptografa um bloco usando AES no modo ECB.
+    """
     cipher = AES.new(key, AES.MODE_ECB)
     return cipher.encrypt(block)
 
+
 def aes_decrypt_block(key, block):
+    """
+    Descriptografa um bloco usando AES no modo ECB.
+    """
     cipher = AES.new(key, AES.MODE_ECB)
     return cipher.decrypt(block)
