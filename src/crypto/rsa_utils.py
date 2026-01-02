928f768f8f068a6b999909aeb8f0a2fdc88329a6
Rubens_Rabelo <rubensrabelo123@gmail.com>
2026-01-02 17:23:56 -0300

    docs(rsa_utils): add docstrings

--- src/crypto/rsa_utils.py
+++ src/crypto/rsa_utils.py
@@ -1,17 +1,28 @@
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
-# Gera par de chaves RSA
+
 def generate_keys():
+    """
+    Gera um par de chaves RSA (privada e pública).
+    """
     key = RSA.generate(2048)
     return key.export_key(), key.publickey().export_key()
 
+
 def encrypt_with_public_key(public_key_bytes, data):
+    """
+    Criptografa dados usando a chave pública RSA.
+    """
     pub = RSA.import_key(public_key_bytes)
     cipher = PKCS1_OAEP.new(pub)
     return cipher.encrypt(data)
 
+
 def decrypt_with_private_key(private_key_bytes, data):
+    """
+    Descriptografa dados usando a chave privada RSA.
+    """
     priv = RSA.import_key(private_key_bytes)
     cipher = PKCS1_OAEP.new(priv)
     return cipher.decrypt(data)
